82044914fc9c04f47f925395a4866edee5aed550
dieselrabbit <37058192+dieselrabbit@users.noreply.github.com>
2018-05-06 22:37:56 -0700

    Moving test app

---
+++ test/aquaconnect_class_test.py
@@ -0,0 +1,23 @@
+import time
+from aquaconnectpy.controller import Controller
+
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
+
+if __name__ == '__main__':
+    from sys import argv
+    myargs = getopts(argv)
+    if '-i' in myargs:  # Example usage.
+        print(myargs['-i'])
+    print(myargs)
+    
+pool = Controller(myargs['-address'], myargs['-interval'])
+#time.sleep(5)
+#pool.switches[5].toggle()
+#time.sleep(5)
+#pool.switches[5].toggle()
